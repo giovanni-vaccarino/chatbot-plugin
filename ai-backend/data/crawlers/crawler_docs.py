cda2e6661c9037c6c129ba47693eb007f08c7c6f
Gianni <giovannivaccarino03@gmail.com>
2025-05-16 19:14:37 +0200

    Updated content extraction for doc crawler.

--- ai-backend/data/crawlers/crawler_docs.py
+++ ai-backend/data/crawlers/crawler_docs.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
+import json
 
 # Home URL of jenkins doc
 BASE_URL = "https://www.jenkins.io/doc/"
@@ -14,17 +15,20 @@ page_content = {}
 # Number of failed links
 failed_links = 0
 
+non_canonic_content_urls = set()
+
 def is_valid_url(url):
     """Check if the URL is a valid link to a new page, internal to the doc, or a redirect to another page"""
     parsed = urlparse(url)
     return parsed.scheme in {"http", "https"} and BASE_URL in url and "#" not in url
 
-def extract_page_content(soup):
-    """Extract content from the 'ctc' div class"""
-    content_div = soup.find("div", class_="ctc")
+def extract_page_content_container(soup):
+    """Extract content from the 'container' div class"""
+    content_div = soup.find("div", class_="container")
     if content_div:
-        return str(content_div) # Keeping html
+        return str(content_div)
     return ""
+
 
 def crawl(url):
     """Recursively crawl documentation pages starting from the base URL"""
@@ -35,18 +39,20 @@ def crawl(url):
     if url in visited_urls:
         return
     
-    print(f"Visiting: {url}")
-    visited_urls.add(url)
-    
+    print(f"Visiting: {url}") 
     try:
+        visited_urls.add(url)
+
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
 
-        content = extract_page_content(soup)
+        #content = extract_page_content(soup)
+        content = extract_page_content_container(soup)
         if content:
             page_content[url] = content
-            # print(page_content[url])
+        else:
+            non_canonic_content_urls.add(url)
         
         # Find all links in the page
         links = soup.find_all("a", href=True)
@@ -67,7 +73,13 @@ def start_crawl():
     print(f"Total pages found: {len(visited_urls)}")
     print(f"Total pages with content: {len(page_content)}")
     print(f"Number of bad links: {failed_links}")
+    print("Non canonic content page structure links:")
+    print(non_canonic_content_urls)
     print("Crawling ended")
 
+    print("Saving results in json")
+    with open("jenkins_docs.json", "w", encoding="utf-8") as f:
+        json.dump(page_content, f, ensure_ascii=False, indent=2)
+        
 if __name__ == "__main__":
     start_crawl()
